29d7c82ecd5269354113f68d9885380f4319c39e
Natali <rinykia@gmail.com>
2015-09-02 15:45:38 +0300

    Add menu

--- barbershop.rb
+++ barbershop.rb
@@ -2,48 +2,19 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
-get '/' do 
-  erb :index
+get '/' do
+  erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"     
 end
 
-get '/create' do
-    @logfile = File.open("users.txt","r")
-    erb :create
-    @logfile.close
+get '/about' do
+	erb :about
 end
 
-post '/' do
-  @user_name = params[:user_name]
-  @phone = params[:phone]
-  @date_time = params[:date_time]
-
-  @title = 'Thank you!'
-  @message = "Dear #{@user_name}, we'll be waiting for you at #{@date_time}"
-  
-  f = File.open './public/users.txt', 'a' #режим а apend, будем добавлять в конец файла(мак и линукс, создать заранее фаил и прописать chmod 777 users.txt)
-  f.write "User: #{@user_name}, Phone: #{@phone}, Date and time: #{@date_time}"
-  erb :message
+get '/visit' do
+	erb :visit
 end
 
-get '/admin' do
-  erb :admin
+get '/contacts' do
+	erb :contacts
 end
 
-post '/admin' do
-  @login = params[:login]
-  @password = params[:password]
-  if @login == 'admin' && @password == 'secret'
-  	return erb :create
-  elsif @login == 'admin' && @password == 'admin'
-  	@message = 'Haha, nice try!Access denied!'
-  	erb :index
-  else 
-  	@message = 'Access denied'
-    erb :index
-  end
-end
-get '/create' do
-    @logfile = File.open("logfile.txt","r")
-    erb :create
-    @logfile.close
-end
---
+++ config.ru
@@ -0,0 +1,2 @@
+require './barbershop.rb'
+run Sinatra::Application
